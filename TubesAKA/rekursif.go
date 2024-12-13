7ecf62d1f34ea40f86ff1cda3dd4b67d93d2209a
Rayvaldord <2311102224@ittelkom-pwt.ac.id>
2024-12-13 22:55:48 +0700

    Tugas Besar Analisis Kompleksitas Algoritma

---
+++ TubesAKA/rekursif.go
@@ -0,0 +1,81 @@
+package main
+import (
+	"fmt"
+	"time"
+)
+
+type City struct {
+	Name     string
+	Distance float64
+}
+
+type TabArray [100]City
+
+func main() {
+	start := time.Now() // Mulai pengukuran waktu
+
+	// Jumlah kota yang ada dalam data
+	var n int = 6
+	// Data kota dan jaraknya
+	var cities TabArray = TabArray{
+		{"Jakarta", 385},
+		{"Bandung", 240},
+		{"Surabaya", 467},
+		{"Medan", 2326},
+		{"Yogyakarta", 164},
+		{"Denpasar", 829},
+		{"Purworejo", 111},
+		{"Malang", 469},
+		{"Bekasi", 367},
+		{"Jayapura", 4860},
+		{"Semarang", 182},
+		{"Banyuwangi", 708},
+		{"Purbalingga", 18},
+		{"Tegal", 101},
+		{"Kebumen", 69},
+		{"Surakarta", 216},
+		{"Cilacap", 49},
+		{"Banjarnegara", 40},
+		{"Wonosobo", 87},
+		{"Lombok", 969},
+		{"Bogor", 361},
+		{"Makassar", 1288},
+		{"Pekanbaru", 1686},
+		{"Manado", 2698},
+		{"Pekalongan", 118},
+		{"Magelang", 136},
+	}
+
+	// Menampilkan data awal sebelum sorting
+	fmt.Println("Data Kota Sebelum Diurutkan:")
+	for i := 0; i < n; i++ {
+		fmt.Printf("%s: %.2f km\n", cities[i].Name, cities[i].Distance)
+	}
+
+	// Sorting menggunakan prosedur khusus
+	sortCities(&cities, n)
+
+	// Menampilkan hasil setelah sorting
+	fmt.Println("------------------------------------")
+	fmt.Println("Setelah Diurutkan Berdasarkan Jarak Terdekat Dari Purwokerto:")
+	for i := 0; i < n; i++ {
+		fmt.Printf("%s: %.2f km\n", cities[i].Name, cities[i].Distance)
+	}
+
+	// Akhir pengukuran waktu
+	end := time.Now()
+	fmt.Printf("Waktu eksekusi: %v\n", end.Sub(start))
+}
+
+func sortCities(cities *TabArray, n int) {
+	if n == 1 {
+		return // Basis rekursi, array dengan 1 elemen sudah terurut
+	}
+	for i := 0; i < n-1; i++ {
+		if cities[i].Distance > cities[i+1].Distance {
+			cities[i], cities[i+1] = cities[i+1], cities[i]
+		}
+	}
+	// Panggil rekursif untuk elemen yang tersisa
+	sortCities(cities, n-1)
+}
